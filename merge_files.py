9e869a5b91e660ba8dca04ba3274e869cfea6abb
Kai B <kai.bruegge@tu-dortmund.de>
2018-09-12 10:44:44 +0200

    parallellize merging of files

--- merge_files.py
+++ merge_files.py
@@ -2,7 +2,11 @@ import fact.io
 import click
 import os
 import glob
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
+from joblib import Parallel, delayed
+
 
 @click.command()
 @click.argument(
@@ -17,7 +21,9 @@ from tqdm import tqdm
         file_okay=True,
     ))
 @click.option('--verify/--no-verify', default=False, help='Wether to verify the file after each append operation')
-def main(input_folder, output_file, verify):
+@click.option('-j', '--n_jobs', default=1, help='number of jobs to use for reading data')
+@click.option('-c', '--chunk_size', default=50, help='files per chunk')
+def main(input_folder, output_file, verify, n_jobs, chunk_size):
     '''
     process multiple simtel files gievn as INPUT_FILES into one hdf5 file saved in OUTPUT_FILE.
     The hdf5 file will contain three groups. 'runs', 'array_events', 'telescope_events'.
@@ -36,19 +42,29 @@ def main(input_folder, output_file, verify):
         click.confirm('Output file exists. Overwrite?', abort=True)
         os.remove(output_file)
 
+    n_chunks = (len(input_files) // chunk_size) + 1
+    chunks = np.array_split(input_files, n_chunks)
+    with Parallel(n_jobs=n_jobs, verbose=1) as parallel:
+        for chunk in tqdm(chunks):
+            results = parallel(delayed(read_file)(f) for f in chunk)
 
-    for f in tqdm(input_files):
+            runs = pd.concat([r[0] for r in results])
+            array_events = pd.concat([r[1] for r in results])
+            telescope_events = pd.concat([r[2] for r in results])
 
-        runs = fact.io.read_data(f, key='runs')
-        array_events = fact.io.read_data(f, key='array_events')
-        telescope_events = fact.io.read_data(f, key='telescope_events')
+            fact.io.write_data(runs, output_file, key='runs', mode='a')
+            fact.io.write_data(array_events, output_file, key='array_events', mode='a')
+            fact.io.write_data(telescope_events, output_file, key='telescope_events', mode='a')
 
-        fact.io.write_data(runs, output_file, key='runs', mode='a')
-        fact.io.write_data(array_events, output_file, key='array_events', mode='a')
-        fact.io.write_data(telescope_events, output_file, key='telescope_events', mode='a')
+            if verify:
+                verify_file(output_file)
 
-        if verify:
-            verify_file(output_file)
+
+def read_file(f):
+    run = fact.io.read_data(f, key='runs')
+    array_events = fact.io.read_data(f, key='array_events')
+    telescope_events = fact.io.read_data(f, key='telescope_events')
+    return run, array_events, telescope_events
 
 
 def verify_file(input_file_path):
